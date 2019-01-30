8a3a518468079999f5a172a0eee7a1d19f1327d7
Alexandre Tuleu <alexandre.tuleu.2005@polytechnique.org>
2019-01-30 15:44:00 +0100

    Implements Helios Messages

---
+++ src-go/arke/helios.go
@@ -0,0 +1,24 @@
+package arke
+
+type HeliosSetPoint struct {
+	Visible uint8
+	UV      uint8
+}
+
+func (c HeliosSetPoint) Marshall(buf []byte) (int, error) {
+	if err := checkSize(buf, 2); err != nil {
+		return 0, err
+	}
+	buf[0] = c.Visible
+	buf[1] = c.UV
+	return 2, nil
+}
+
+func (c *HeliosSetPoint) Unmarshall(buf []byte) error {
+	if err := checkSize(buf, 2); err != nil {
+		return err
+	}
+	c.Visible = buf[0]
+	c.UV = buf[1]
+	return nil
+}
