69ed899e88346260761ba0e9aae3fe8a073eb54d
Alexandre Tuleu <alexandre.tuleu.2005@polytechnique.org>
2019-03-20 10:10:42 +0100

    Fixes important race condition in systime

--- src-avr/config.h
+++ src-avr/config.h
@@ -7,8 +7,8 @@
 		TCCR0A = _BV(WGM01); \
 		TCCR0B = _BV(CS01) | _BV(CS00); \
 	}while(0)
-#define OCIE0A_ENABLE() TIMSK0 = _BV(OCIE0A)
-#define OCIE0A_DISABLE() TIMSK0 = _BV(OCIE0A)
+#define OCIE0A_ENABLE() TIMSK0 |= _BV(OCIE0A)
+#define OCIE0A_DISABLE() TIMSK0 &= ~(_BV(OCIE0A))
 #define LIBARKE_TIMER0_COMPA_VECT TIMER0_COMPA_vect
 #elif defined(__AVR_AT90CAN32__) \
 	|| defined(__AVR_AT90CAN64__) \
@@ -16,8 +16,8 @@
 #define START_TIMER0_1ms() do{	  \
 		TCCR0A = _BV(WGM01) | _BV(CS01) | _BV(CS00); \
 	}while(0)
-#define OCIE0A_ENABLE() TIMSK0 = _BV(OCIE0A)
-#define OCIE0A_DISABLE() TIMSK0 = _BV(OCIE0A)
+#define OCIE0A_ENABLE() TIMSK0 |= _BV(OCIE0A)
+#define OCIE0A_DISABLE() TIMSK0 &= ~(_BV(OCIE0A))
 #define LIBARKE_TIMER0_COMPA_VECT TIMER0_COMP_vect
 #else
 #error "Unssuported AVR device"
