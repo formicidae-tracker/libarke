551f59a89dcae24d49a99a019c5dc47b93bda440
Alexandre Tuleu <alexandre.tuleu.2005@polytechnique.org>
2025-02-25 21:27:40 +0100

    Adds code and structure for notus.

--- include/arke.h
+++ include/arke.h
@@ -1,54 +1,55 @@
 #pragma once
 #include "inttypes.h"
+#include <cstdint>
 
 #ifdef __cplusplus
-extern "C"{
+extern "C" {
 #endif //__cplusplus
-
 
 typedef enum ArkeMessageType_e {
 	ARKE_NETWORK_CONTROL_COMMAND = 0x00,
-	ARKE_HIGH_PRIORITY_MESSAGE = 0x01,
-	ARKE_MESSAGE = 0x02,
-	ARKE_HEARTBEAT = 0x03,
-	ARKE_MESSAGE_TYPE_MASK = 0x03 << 9
+	ARKE_HIGH_PRIORITY_MESSAGE   = 0x01,
+	ARKE_MESSAGE                 = 0x02,
+	ARKE_HEARTBEAT               = 0x03,
+	ARKE_MESSAGE_TYPE_MASK       = 0x03 << 9
 } ArkeMessageType;
 
 typedef enum ArkeNodeClass_e {
-	ARKE_BROADCAST = 0x0,
-	ARKE_ZEUS = 0x38,
-	ARKE_HELIOS = 0x34,
-	ARKE_CELAENO = 0x30,
+	ARKE_BROADCAST       = 0x0,
+	ARKE_ZEUS            = 0x38,
+	ARKE_HELIOS          = 0x34,
+	ARKE_CELAENO         = 0x30,
+	ARKE_NOTUS           = 0x2c,
 	ARKE_NODE_CLASS_MASK = 0x3f << 3
 } ArkeNodeClass;
 
-
 typedef enum ArkeNetworkCommand_e {
-	ARKE_RESET_REQUEST = 0x00,
-	ARKE_SYNCHRONISATION = 0x01,
+	ARKE_RESET_REQUEST     = 0x00,
+	ARKE_SYNCHRONISATION   = 0x01,
 	ARKE_ID_CHANGE_REQUEST = 0x02,
-	ARKE_ERROR_REPORT = 0x03,
+	ARKE_ERROR_REPORT      = 0x03,
 	ARKE_HEARTBEAT_REQUEST = 0x07,
-	ARKE_SUBID_MASK = 0x07
+	ARKE_SUBID_MASK        = 0x07
 } ArkeNetworkCommand;
 
 //#define ARKE_SUBID_MASK ARKE_HEARTBEAT_REQUEST
 
 typedef enum ArkeMessageClass_e {
-	ARKE_ZEUS_SET_POINT = 0x38,
-	ARKE_ZEUS_REPORT = 0x39,
-	ARKE_ZEUS_VIBRATION_REPORT = 0x3a,
-	ARKE_ZEUS_CONFIG = 0x3b,
-	ARKE_ZEUS_STATUS = 0x3c,
-	ARKE_ZEUS_CONTROL_POINT = 0x3d,
+	ARKE_ZEUS_SET_POINT         = 0x38,
+	ARKE_ZEUS_REPORT            = 0x39,
+	ARKE_ZEUS_VIBRATION_REPORT  = 0x3a,
+	ARKE_ZEUS_CONFIG            = 0x3b,
+	ARKE_ZEUS_STATUS            = 0x3c,
+	ARKE_ZEUS_CONTROL_POINT     = 0x3d,
 	ARKE_ZEUS_DELTA_TEMPERATURE = 0x3e,
-	ARKE_HELIOS_SET_POINT = 0x34,
-	ARKE_HELIOS_PULSE_MODE = 0x35,
-	ARKE_CELAENO_SET_POINT = 0x30,
-	ARKE_CELAENO_STATUS = 0x31,
-	ARKE_CELAENO_CONFIG = 0x32
+	ARKE_HELIOS_SET_POINT       = 0x34,
+	ARKE_HELIOS_PULSE_MODE      = 0x35,
+	ARKE_CELAENO_SET_POINT      = 0x30,
+	ARKE_CELAENO_STATUS         = 0x31,
+	ARKE_CELAENO_CONFIG         = 0x32,
+	ARKE_NOTUS_SET_POINT        = 0x2c,
+	ARKE_NOTUS_CONFIG           = 0x2d,
 } ArkeMessageClass;
-
 
 struct ArkeZeusSetPoint_t {
 	uint16_t Humidity;
@@ -57,23 +58,21 @@ struct ArkeZeusSetPoint_t {
 } __attribute__((packed));
 typedef struct ArkeZeusSetPoint_t ArkeZeusSetPoint;
 
-
 struct ArkeZeusReport_t {
-	uint16_t Humidity:14;
-	uint16_t Temperature1:14;
-	uint16_t Temperature2:12;
-	uint16_t Temperature3:12;
-	uint16_t Temperature4:12;
+	uint16_t Humidity : 14;
+	uint16_t Temperature1 : 14;
+	uint16_t Temperature2 : 12;
+	uint16_t Temperature3 : 12;
+	uint16_t Temperature4 : 12;
 } __attribute__((packed));
 typedef struct ArkeZeusReport_t ArkeZeusReport;
-
 
 struct ArkePIDConfig_t {
 	uint8_t ProportionalMult;
 	uint8_t DerivativeMult;
 	uint8_t IntegralMult;
-	uint8_t DividerPower:4;
-	uint8_t DividerPowerInt:4;
+	uint8_t DividerPower : 4;
+	uint8_t DividerPowerInt : 4;
 } __attribute__((packed));
 
 typedef struct ArkePIDConfig_t ArkePIDConfig;
@@ -87,14 +86,15 @@ typedef struct ArkeZeusConfig_t ArkeZeusConfig;
 
 #define ARKE_FAN_AGING_ALERT (0x4000)
 #define ARKE_FAN_STALL_ALERT (0x8000)
-#define ARKE_FAN_RPM_MASK (0x3fff)
+#define ARKE_FAN_RPM_MASK    (0x3fff)
 
-#define ArkeFanAging(status) (((status).fanStatus & ARKE_FAN_AGING_ALERT) != 0x0000)
-#define ArkeFanStall(status) (((status).fanStatus & ARKE_FAN_STALL_ALERT) != 0x0000)
-#define ArkeFanRPM(status) ( (status).fanStatus & ARKE_FAN_RPM_MASK )
+#define ArkeFanAging(status)                                                   \
+	(((status).fanStatus & ARKE_FAN_AGING_ALERT) != 0x0000)
+#define ArkeFanStall(status)                                                   \
+	(((status).fanStatus & ARKE_FAN_STALL_ALERT) != 0x0000)
+#define ArkeFanRPM(status) ((status).fanStatus & ARKE_FAN_RPM_MASK)
 
 typedef uint16_t ArkeFanStatus;
-
 
 typedef enum ArkeZeusStatus_e {
 	ARKE_ZEUS_IDLE                    = 0,
@@ -110,20 +110,16 @@ struct ArkeZeusStatus_t {
 } __attribute__((packed));
 typedef struct ArkeZeusStatus_t ArkeZeusStatus;
 
-
 struct ArkeZeusControlPoint_t {
 	int16_t Humidity;
 	int16_t Temperature;
 } __attribute__((packed));
 typedef struct ArkeZeusControlPoint_t ArkeZeusControlPoint;
 
-
 struct ArkeZeusDeltaTemperature_t {
 	int16_t Delta[4];
 } __attribute__((packed));
 typedef struct ArkeZeusDeltaTemperature_t ArkeZeusDeltaTemperature;
-
-
 
 struct ArkeHeliosSetPoint_t {
 	uint8_t Visible;
@@ -134,27 +130,29 @@ typedef struct ArkeHeliosSetPoint_t ArkeHeliosSetPoint;
 struct ArkeCelaenoSetPoint_t {
 	uint8_t Power;
 } __attribute__((packed));
-typedef struct ArkeCelaenoSetPoint_t  ArkeCelaenoSetPoint;
+typedef struct ArkeCelaenoSetPoint_t ArkeCelaenoSetPoint;
 
 struct ArkeCelaenoStatus_t {
-	uint8_t  waterLevel;
+	uint8_t       waterLevel;
 	ArkeFanStatus fanStatus;
 } __attribute__((packed));
-typedef struct ArkeCelaenoStatus_t  ArkeCelaenoStatus;
-
+typedef struct ArkeCelaenoStatus_t ArkeCelaenoStatus;
 
 typedef enum ArkeCelaenoWaterLevel_e {
-	ARKE_CELAENO_NOMINAL = 0,
-	ARKE_CELAENO_WARNING = (1 << 0),
+	ARKE_CELAENO_NOMINAL  = 0,
+	ARKE_CELAENO_WARNING  = (1 << 0),
 	ARKE_CELAENO_CRITICAL = (1 << 1),
 	ARKE_CELAENO_RO_ERROR = (1 << 2)
 } ArkeCelaenoWaterLevel;
 
-#define ArkeCelaenoWaterNominal(status) ( (status).waterLevel == 0 )
-#define ArkeCelaenoWaterWarning(status) ( (status).waterLevel == ARKE_CELAENO_WARNING )
-#define ArkeCelaenoWaterCritical(status) ( ((status).waterLevel & ~(ARKE_CELAENO_CRITICAL | ARKE_CELAENO_RO_ERROR) ) == ARKE_CELAENO_WARNING )
-#define ArkeCelaenoWaterHasRoError(status) (((status).waterLevel & ARKE_CELAENO_RO_ERROR) != 0x00)
-
+#define ArkeCelaenoWaterNominal(status) ((status).waterLevel == 0)
+#define ArkeCelaenoWaterWarning(status)                                        \
+	((status).waterLevel == ARKE_CELAENO_WARNING)
+#define ArkeCelaenoWaterCritical(status)                                       \
+	(((status).waterLevel & ~(ARKE_CELAENO_CRITICAL | ARKE_CELAENO_RO_ERROR)   \
+	 ) == ARKE_CELAENO_WARNING)
+#define ArkeCelaenoWaterHasRoError(status)                                     \
+	(((status).waterLevel & ARKE_CELAENO_RO_ERROR) != 0x00)
 
 struct ArkeCelaenoConfig_t {
 	uint16_t RampUpTimeMS;
@@ -165,7 +163,11 @@ struct ArkeCelaenoConfig_t {
 
 typedef struct ArkeCelaenoConfig_t ArkeCelaenoConfig;
 
-
+struct ArkeNotusConfig_t {
+	uint16_t RampUpTimeMS;
+	uint16_t RampDownTimeMS;
+	uint16_t MinOnTimeMS;
+} __attribute__((packed));
 
 #ifdef __cplusplus
 }
